a6119460a26bf88448203428768f1e320c8b0839
Maksim Vakarchuk <11082252+maks112v@users.noreply.github.com>
2023-10-21 02:11:37 -0400

    adding initial view

--- Things/Views/LoginView.swift
+++ Things/Views/LoginView.swift
@@ -8,8 +8,40 @@
 import SwiftUI
 
 struct LoginView: View {
+    
+    @State var email = ""
+    @State var password = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack {
+                // Header
+                HeaderView()
+                // Login Form
+                Form {
+                    TextField("Email Address", text: $email)
+                        .textFieldStyle(DefaultTextFieldStyle())
+                    SecureField("Password", text: $password)
+                        .textFieldStyle(DefaultTextFieldStyle())
+                    
+                    Button {
+                        // Attempt Login
+                    } label: {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 10).foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                            Text("Log In").foregroundColor(.white).bold()
+                        }
+                    }
+                }
+                
+                // Create Account
+                VStack {
+                    Text("New around here?")
+                    NavigationLink("Create an Account", destination: RegisterView())
+                }
+                .padding(.bottom, 50)
+            }
+        }
     }
 }
 
